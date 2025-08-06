6fdd07218c4064df90ab9e31fc88a6aacb8b5893
Priti Gupta <priticse2633@gmail.com>
2025-08-06 10:58:37 +0530

    upadted

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Pagination from './Components/Pagination'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+         <h1>Employee Data Tables</h1>
+        <Pagination />
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
